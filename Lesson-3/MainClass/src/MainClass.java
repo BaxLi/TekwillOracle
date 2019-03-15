9d1c07ad275885b05a919586e8e3d29a9ef01d37
BaxLi <lilian.baxan@gmail.com>
2019-03-15 19:36:26 +0200

    change 1

--- Lesson-3/MainClass/src/MainClass.java
+++ Lesson-3/MainClass/src/MainClass.java
@@ -8,9 +8,16 @@ public class MainClass {
   /**
    * @param args the command line arguments
    */
+  
   public static void main(String[] args) {
     // TODO code application logic here
     System.out.println("Hello");
+    
+    for (String as: args) {
+          System.out.println(as);
+    }
+   
     System.out.println(args[1]);
+  
   }
 }
